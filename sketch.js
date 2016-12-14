864340f23e1dc113367f0ea946aa0cffd095eff4
Prathamesh Kawale <kawa5@live.com>
2016-12-14 01:43:02 -0800

    adding wall

--- sketch.js
+++ sketch.js
@@ -1,14 +1,21 @@
 var bird;
+var walls = [];
 
 function setup() {
 	createCanvas(400, 600);
 	bird = new Bird();
+	walls.push(new Wall());
 }
 
 function draw(){
 	background(51);
 	bird.update();
 	bird.show();
+
+	for(var i = 0; i < walls.length; i++){
+		walls[i].show();
+		walls[i].update();
+	}
 }
 
 function keyPressed() {
--- wall.js
+++ wall.js
@@ -1,9 +1,16 @@
 function Wall(){
 	this.top = random(height/2);
 	this.bottom = random(height/2);
+	this.x = width;
+	this.speed = 5;
 
-	this.show(){
+	this.show = function(){
 		fill(255);
-		rect();
+		rect(this.x, 0, 20, this.top);
+		rect(this.x, height - this.bottom, 20, this.bottom);
+	}
+
+	this.update = function(){
+		this.x = this.x - this.speed;
 	}
 }
